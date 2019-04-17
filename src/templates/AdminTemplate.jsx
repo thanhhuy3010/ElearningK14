37ce11d7379074c949db50376ebb7232a35356c8
thanhhuy3010 <huyhuythanh3010@gmail.com>
2019-04-17 19:07:21 +0700

    save all

--- src/templates/AdminTemplate.jsx
+++ src/templates/AdminTemplate.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 import AdminPage from '../Pages/AdminPage';
+import '../Styles/AdminPage.css'
 import LoginAdminPage from '../Pages/LoginAdminPage';
 class AdminTemplate extends Component {
   render() {
